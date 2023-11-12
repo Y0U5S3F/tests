b9035ffd76153c25b1008ad0053c94698af4c79e
Youssef Saidani <akim25486@gmail.com>
2023-11-12 08:43:58 +0100

    html

---
+++ java/rev/rev.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class rev {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String titre = new String();
+        titre = sc.nextLine();
+        System.out.println("your title is : "+titre);
+        sc.close();
+    }
+}
--- java/tests.java
+++ java/tests.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class tests {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("input the string");
-        String test = sc.nextLine();
-        if(test.equals("boom baby")){
-            System.out.println("sexy lady");
+        String[] x = new String[10];
+        int i;
+        for(i=0;i<10;i++){
+            System.out.printf("Donner Tab[%d]%n",i);
+            x[i] = sc.nextLine();
         }
-        else{
-            System.out.println("your string was: "+test);
+        for(i=0;i<10;i++){
+            System.out.println(x[i]);
         }
-        sc.close();
     }
 }
